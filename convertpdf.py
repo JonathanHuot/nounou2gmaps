f60596614a518940bae26c8cc62a9dc5a8247ae0
Jonathan Huot <jonathan.huot@gmail.com>
2015-05-28 23:38:04 +0200

    Added python requirements

---
+++ convertpdf.py
@@ -0,0 +1,128 @@
+//
+// Python script to try if PDF can be converted automatically.
+// With current List.pdf, that's not possible !! 
+//
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+
+def convert_pdf_to_txt(path):
+    from pdfminer.converter import TextConverter
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+def convert_pdf_to_html(path):
+    from pdfminer.converter import HTMLConverter
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+def convert_pdf_to_xml(path):
+    from pdfminer.converter import XMLConverter
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = XMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+  
+def convert_pdf_to_csv(path):
+    from cStringIO import StringIO  
+    from pdfminer.converter import LTChar, TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfparser import PDFDocument, PDFParser
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.converter import TextConverter
+    class CsvConverter(TextConverter):
+        def __init__(self, *args, **kwargs):
+            TextConverter.__init__(self, *args, **kwargs)
+
+        def end_page(self, i):
+            from collections import defaultdict
+            lines = defaultdict(lambda : {})
+            for child in self.cur_item._objs:                #<-- changed
+                if isinstance(child, LTChar):
+                    (_,_,x,y) = child.bbox                   
+                    line = lines[int(-y)]
+                    line[x] = child._text.encode(self.codec) #<-- changed
+
+            for y in sorted(lines.keys()):
+                line = lines[y]
+                self.outfp.write(";".join(line[x] for x in sorted(line.keys())))
+                self.outfp.write("\n")
+
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = CsvConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
